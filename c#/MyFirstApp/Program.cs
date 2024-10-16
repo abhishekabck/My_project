3e846b0f6e032fd3f1710df76ad411de962e2265
Abhishek Chaurasiya <abhiabckchaurasiya@gmail.com>
2024-10-16 19:52:39 +0530

    jh

---
+++ c#/MyFirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+class HelloWorld
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Hello, World!");
+    }
+}
---
+++ c#/code/Firstprg.cs
@@ -0,0 +1,6 @@
+// This is My First program in c#
+class HelloWorld{
+    static void Main() {
+        System.Console.WriteLine("Hello, World!");
+    }
+}
