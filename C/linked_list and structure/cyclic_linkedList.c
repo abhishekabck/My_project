9cbe75d50e8bf179cfe98b853c7f4fdeab37b754
abhishekabck <111350800+abhishekabck@users.noreply.github.com>
2024-10-24 09:33:17 +0000

    2343

--- C/linked_list and structure/cyclic_linkedList.c
+++ C/linked_list and structure/cyclic_linkedList.c
@@ -6,6 +6,19 @@ struct node {
     struct node* next;
 };
 
+struct CircularLList{
+    struct node* head;
+    struct node* tail;
+
+    //  Function pointers
+    void (*append)(struct CircularLList*, int);
+    void (*insertAtBeg)(struct CircularLList*, int);
+    void (*traverse)(struct CircularLList*);
+
+};
+
+
+
 struct node* __newNode__(int data){
     struct node* otp = (struct node*)malloc(sizeof(struct node));
     otp->data = data;
@@ -13,65 +26,86 @@ struct node* __newNode__(int data){
     return otp;
 }
 
-struct node* last(struct node* head){
-    if (head == NULL){
-        return NULL;
-    }
-    struct node* temp = head;
-    while (temp->next != head){
-        temp = temp->next;
-    }
-    return temp;
-}
 
-
-void traverse(struct node* head){
-    if (head == NULL){
-        printf("Linked is EMpty");
+void traverse(struct CircularLList* list) {
+    if (list->head == NULL) {
+        printf("Linked list is empty\n");
         return;
     }
-    struct node* temp = head;
-    while (temp->next != head){
+    struct node* temp = list->head;
+    do {
         printf("%d -> ", temp->data);
         temp = temp->next;
-    }
-    printf("%d",temp->data);
+    } while (temp != list->head);
+    printf("(back to head)\n");
 }
 
-void insertAtBeg(struct node** head, int data){
-    struct node* t1 = __newNode__(data);
-    if (*head == NULL){
-        *head = t1;
-        t1->next = t1;
-        return;
-    }
-    else if ((*head)->next == *head){
-        t1->next = *head;
-        (*head)->next = t1;
-        *head = t1;
-    }
-    else {
-        struct node* temp = *head;
-        while (temp->next!=*head){
-            temp = temp->next;
-        }
-        temp->next = t1;
-        t1->next = *head;
-        *head = t1;
+void insertAtBeg(struct CircularLList* list, int data) {
+    struct node* newNode = __newNode__(data);
+    if (list->head == NULL) {
+        list->head = newNode;
+        list->tail = newNode;
+        newNode->next = newNode; // Point to itself
+    } else {
+        newNode->next = list->head;
+        list->head = newNode;
+        list->tail->next = list->head; // Update the tail's next
     }
 }
 
-void 
+void append(struct CircularLList* list, int data) {
+    struct node* newNode = __newNode__(data);
+    if (list->tail == NULL) {
+        list->head = newNode;
+        list->tail = newNode;
+        newNode->next = newNode; // Point to itself
+    } else {
+        list->tail->next = newNode;
+        newNode->next = list->head; // Point to head
+        list->tail = newNode; // Update tail
+    }
+}
 
-int main(){
-    // Initialize
-    struct node* head = NULL;
-    insertAtBeg(&head, 235);
-    insertAtBeg(&head, 452);
-    insertAtBeg(&head, 453);
-    insertAtBeg(&head, 4523);
-    insertAtBeg(&head, 435);
-    insertAtBeg(&head, 523);
-    traverse(head);
+
+struct CircularLList* create_CLL() {
+    struct CircularLList* list = (struct CircularLList*)malloc(sizeof(struct CircularLList));
+    list->head = NULL;
+    list->tail = NULL;
+    list->append = append;
+    list->insertAtBeg = insertAtBeg;
+    list->traverse = traverse;
+    return list;
+}
+
+// int main(){
+//     // Initialize
+//     struct node* head = NULL;
+//     struct node* tail = NULL;
+//     append(&head, &tail, 235);
+//     append(&head, &tail, 452);
+//     append(&head, &tail, 453);
+//     insertAtBeg(&head, &tail, 4523);
+//     append(&head, &tail, 435);
+//     append(&head, &tail, 523);
+//     insertAtBeg(&head, &tail, 9712);
+//     traverse(head);
+//     return 0;
+// }
+
+int main() {
+    // Create circular linked list
+    struct CircularLList* list = create_CLL();
+    
+    // Use the methods
+    list->append(list, 235);
+    list->append(list, 452);
+    list->insertAtBeg(list, 9712);
+    list->append(list, 453);
+    list->insertAtBeg(list, 4523);
+    list->traverse(list);
+    
+    // Free memory (optional, but recommended for larger programs)
+    free(list); // Add proper memory management for nodes if necessary
+    
     return 0;
 }
